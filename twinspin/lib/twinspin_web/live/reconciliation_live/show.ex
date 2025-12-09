fddca0d2329ee4d64eb8a698ae0f77fbcc8390b9
thanos <thanosv@gmail.com>
2025-12-09 20:16:17 +0000

    add explicit render function for template

--- twinspin/lib/twinspin_web/live/reconciliation_live/show.ex
+++ twinspin/lib/twinspin_web/live/reconciliation_live/show.ex
@@ -12,6 +12,12 @@ defmodule TwinspinWeb.ReconciliationLive.Show do
   import Ecto.Query
 
   @impl true
+  def render(assigns) do
+    ~H"""
+    """
+  end
+
+  @impl true
   def mount(%{"id" => id}, _session, socket) do
     if connected?(socket) do
       Phoenix.PubSub.subscribe(Twinspin.PubSub, "reconciliation_jobs")
