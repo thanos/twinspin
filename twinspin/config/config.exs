2f72bf78461773c1aa9f87e07c8d0a9061b2173e
thanos <thanosv@gmail.com>
2025-12-09 03:28:41 +0000

    use PG notifier for oban

--- twinspin/config/config.exs
+++ twinspin/config/config.exs
@@ -25,7 +25,7 @@ config :twinspin, TwinspinWeb.Endpoint,
 # Configure Oban
 config :twinspin, Oban,
   repo: Twinspin.Repo,
-  notifier: {Oban.Notifiers.Postgres, []},
+  notifier: {Oban.Notifiers.PG, []},
   queues: [reconciliation: 10],
   plugins: [Oban.Plugins.Pruner]
 
