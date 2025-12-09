902934a4ae91d7a0c41757302b54e811e238d58b
thanos <thanosv@gmail.com>
2025-12-09 03:26:14 +0000

    switch to postgres

--- twinspin/mix.exs
+++ twinspin/mix.exs
@@ -37,7 +37,7 @@ defmodule Twinspin.MixProject do
       {:phoenix, "~> 1.8.0-rc.4", override: true},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
-      {:ecto_sqlite3, ">= 0.0.0"},
+      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.0-rc.0"},
