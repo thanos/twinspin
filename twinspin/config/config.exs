1544296617868bf2c0d4f6bcbf16a35800e84afa
thanos <thanosv@gmail.com>
2025-12-09 03:16:01 +0000

    fix: remove trailing comma in logger config

--- twinspin/config/config.exs
+++ twinspin/config/config.exs
@@ -54,8 +54,7 @@ config :tailwind,
 
 # Configures Elixir's Logger
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  
+  format: "$time $metadata[$level] $message\n"
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
@@ -63,4 +62,3 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
-
