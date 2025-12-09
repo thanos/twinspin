ea48c35b2c31fb4f62ff3a8fd8aa80f56e49bff0
thanos <thanosv@gmail.com>
2025-12-09 00:15:17 +0000

    add ReconciliationLive.Index route

--- twinspin/lib/twinspin_web/router.ex
+++ twinspin/lib/twinspin_web/router.ex
@@ -17,7 +17,7 @@ defmodule TwinspinWeb.Router do
   scope "/", TwinspinWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", ReconciliationLive.Index, :index
   end
 
   # Other scopes may use custom stacks.
